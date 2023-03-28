af66a98dd99b48ebf866c9193f1778ecbeab6473
Sanjeev Lakra <117858199+Sanjeevlakra@users.noreply.github.com>
2023-03-29 00:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const panels=document.querySelectorAll(".panel");
+
+	panels.forEach((panel)=> {
+		panel.addEventListener("click",() => {
+			panels.forEach((panel)=>{
+				panel.classList.remove("active");
+			});
+			panel.classList.remove("active");
+		});
+	});
